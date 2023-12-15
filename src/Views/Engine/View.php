2a5a23fc2e22f32509926b67e6b46e69397e9131
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-15 21:56:53 +0100

    Created View class

---
+++ src/Views/Engine/View.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MvcLite\Views\Engine;
+
+use MvcLite\Views\Engine\Exceptions\NotFoundViewException;
+
+class View
+{
+    public static function render(string $viewPath, array $props = [])
+    {
+        $absoluteViewPath = "./src/Viewsc/$viewPath.php";
+
+        if (!file_exists($absoluteViewPath))
+        {
+            $error = new NotFoundViewException();
+            $error->render();
+        }
+
+        extract($props);
+
+        include_once $absoluteViewPath;
+    }
+}
