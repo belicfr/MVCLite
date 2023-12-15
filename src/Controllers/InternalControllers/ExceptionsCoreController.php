bbcfc4b7ef9eda2fce33b22293116e029cf03b6c
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-15 21:56:15 +0100

    Created core exceptions controller

---
+++ src/Controllers/InternalControllers/ExceptionsCoreController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MvcLite\Controllers\InternalControllers;
+
+use MvcLite\Engine\InternalResources\Storage;
+
+/**
+ * Controller used by core exceptions.
+ *
+ * @author belicfr
+ */
+class ExceptionsCoreController
+{
+    public function __construct()
+    {
+        // Empty constructor.
+    }
+
+    /**
+     * Render MVCLite exception dialog CSS file.
+     */
+    public function renderingCss()
+    {
+        $cssFilePath = Storage::getEngineInternalResourcesPath() . "/ExceptionRendering/rendering.css";
+        include_once $cssFilePath;
+    }
+}
