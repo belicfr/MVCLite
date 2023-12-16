11dd9871f2f9bbac355f9b1e63da2d0fc8b1c57e
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-16 03:12:36 +0100

    Replaced TestController by cleaned HelloController

---
+++ src/Controllers/HelloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MvcLite\Controllers;
+
+class HelloController
+{
+    public function __construct()
+    {
+        // Empty constructor.
+    }
+
+    // Create your own methods!
+}
