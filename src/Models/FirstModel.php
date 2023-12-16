79725e857a2929f456b02363bf452ac605b57c55
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-16 17:29:27 +0100

    Created default example model class

---
+++ src/Models/FirstModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MvcLite\Models;
+
+class FirstModel
+{
+    public function __construct(string $brand, string $seats)
+    {
+        // Empty constructor.
+    }
+
+    // Create your own methods.
+}
