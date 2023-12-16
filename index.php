a7715a9dac3d785ab4abab1e78cdbe636ff4cd91
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-16 03:10:32 +0100

    Exceptions and debug CSS files imported globally

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
+use MvcLite\Engine\InternalResources\Storage;
 use MvcLite\Router\Engine\Exceptions\NoneRouteException;
 use MvcLite\Router\Engine\Router;
 
 require_once "vendor/autoload.php";
+
+$debugCss = file_get_contents(Storage::getEnginePath()
+    . "/DevelopmentUtilities/DebugRendering/rendering.css");
+
+$exceptionsCss = file_get_contents(Storage::getEnginePath()
+    . "/InternalResources/ExceptionRendering/rendering.css");
+
+echo "<style>$exceptionsCss $debugCss</style>";
+
 require_once "src/Router/reserved.php";
 require_once "src/Router/routes.php";
 
@@ -17,3 +27,4 @@ const ROUTE_PATH_PREFIX = '/';
 
 $route = Router::getRouteByPath(ROUTE_PATH_PREFIX . $_GET["route"]);
 Router::useRoute($route);
+
