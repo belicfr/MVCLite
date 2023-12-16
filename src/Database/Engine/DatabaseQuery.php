1298e07e4046a616b07d4481e42b67361b4143f0
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-16 21:16:06 +0100

    Created DatabaseQuery class

---
+++ src/Database/Engine/DatabaseQuery.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace MvcLite\Database\Engine;
+
+use MvcLite\Database\Engine\Exceptions\FailedConnectionToDatabaseException;
+use MvcLite\Engine\DevelopmentUtilities\Debug;
+use PDOException;
+use PDOStatement;
+
+class DatabaseQuery
+{
+    /** Current SQL query. */
+    private string $sqlQuery;
+
+    /** Current SQL query parameters. */
+    private array $parameters;
+
+    /** Current SQL query preparation. */
+    private PDOStatement $preparation;
+
+    public function __construct(string $sqlQuery, array $parameters)
+    {
+        $this->sqlQuery = $sqlQuery;
+        $this->parameters = $parameters;
+
+        try
+        {
+            $this->prepareQuery();
+            $this->executeQuery();
+        }
+        catch (PDOException $e)
+        {
+            $error = new FailedConnectionToDatabaseException();
+            $error->render();
+        }
+    }
+
+    /**
+     * Prepare SQL query.
+     *
+     * @return PDOStatement Prepared SQL query PDO object
+     */
+    private function prepareQuery(): PDOStatement
+    {
+        global $db;
+
+        return $this->preparation = $db->prepare($this->sqlQuery);
+    }
+
+    /**
+     * Execute SQL query.
+     *
+     * @return bool PDO execution state
+     */
+    private function executeQuery(): bool
+    {
+        global $db;
+
+        return $this->preparation
+                    ->execute($this->parameters);
+    }
+
+    /**
+     * @return array All SQL query results
+     */
+    public function getAll(): array
+    {
+        return $this->preparation->fetchAll();
+    }
+}
