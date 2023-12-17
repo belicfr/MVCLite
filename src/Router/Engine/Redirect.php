b8c9e45718f2fdd014bf5dec4a7009d095efecd5
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-17 15:20:42 +0100

    Created Redirect::route() method

--- src/Router/Engine/Redirect.php
+++ src/Router/Engine/Redirect.php
@@ -13,10 +13,27 @@ class Redirect
      * Redirection by route path.
      *
      * @param string $path Route path
+     * @return Redirect Redirect object
      */
     public static function to(string $path): Redirect
     {
         $route = Router::getRouteByPath($path);
+
+        $redirection = new Redirect($route);
+        $redirection->redirect();
+
+        return $redirection;
+    }
+
+    /**
+     * Redirection by route name.
+     *
+     * @param string $name Route name
+     * @return Redirect Redirect object
+     */
+    public static function route(string $name): Redirect
+    {
+        $route = Router::getRouteByName($name);
 
         $redirection = new Redirect($route);
         $redirection->redirect();
