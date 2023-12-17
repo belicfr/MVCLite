d5d4161b462f8aa2fe211aeeb3743819ef66d11b
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-17 03:26:28 +0100

    Added example method to example controller

--- src/Controllers/HelloController.php
+++ src/Controllers/HelloController.php
@@ -3,7 +3,10 @@
 namespace MvcLite\Controllers;
 
 use MvcLite\Controllers\Engine\Controller;
+use MvcLite\Engine\InternalResources\Storage;
+use MvcLite\Router\Engine\Redirect;
 use MvcLite\Router\Engine\Request;
+use MvcLite\Views\Engine\View;
 
 class HelloController extends Controller
 {
@@ -14,5 +17,15 @@ class HelloController extends Controller
         // Empty constructor.
     }
 
-    // Create your own methods!
+    // Create your own methods!.
+
+    public function redirectToIndex(): void
+    {
+        Redirect::to("/index");
+    }
+
+    public function render(Request $request)
+    {
+        View::render("HelloWorld");
+    }
 }
