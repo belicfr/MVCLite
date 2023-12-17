1e755b6159370c031324388341d7569443bc6ddc
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-17 03:26:36 +0100

    Created an example route

--- src/Router/routes.php
+++ src/Router/routes.php
@@ -9,3 +9,7 @@
 use MvcLite\Router\Engine\Router;
 
 // Create your own routes!
+
+Router::get("/", \MvcLite\Controllers\HelloController::class, "redirectToIndex");
+
+Router::get("/index", \MvcLite\Controllers\HelloController::class, "render");
