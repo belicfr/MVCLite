9d007f6de57e251684a3637f81c402c1166b0f78
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-17 15:20:21 +0100

    Created Session class

---
+++ src/Engine/Session/Session.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace MvcLite\Engine\Session;
+
+use MvcLite\Database\Engine\Database;
+use MvcLite\Engine\DevelopmentUtilities\Debug;
+use MvcLite\Engine\Security\Password;
+
+/**
+ * Session manager class.
+ *
+ * @author belicfr
+ */
+class Session
+{
+    /** $_SESSION key for current session id. */
+    private const AUTH_SESSION_VARIABLE = "AUTH_SESSION_ID";
+
+    /**
+     * @return bool If visitor has a session id
+     */
+    public static function isLogged(): bool
+    {
+        return isset($_SESSION[self::AUTH_SESSION_VARIABLE]);
+    }
+
+    /**
+     * @return int|false Current session id if logged;
+     *                   else FALSE
+     */
+    public static function getSessionId(): int|false
+    {
+        return self::isLogged()
+            ? $_SESSION[self::AUTH_SESSION_VARIABLE]
+            : false;
+    }
+
+    public static function attemptLogin(string $login, string $password): bool
+    {
+        $query = "SELECT * FROM users WHERE "
+                . AUTHENTIFICATION_COLUMNS["login"]
+                . " = ?";
+
+        $user = Database::query($query, $login)
+            ->get();
+
+        $attemptState
+            = $user
+              && Password::verify($password,
+                                  $user[AUTHENTIFICATION_COLUMNS["password"]]);
+
+        if ($attemptState)
+        {
+            self::setSessionId($user["id"]);
+        }
+
+        return $attemptState;
+    }
+
+    /**
+     * Force to log in with given user id.
+     *
+     * @param int $sessionId Session id
+     */
+    private static function setSessionId(int $sessionId): void
+    {
+        $_SESSION[self::AUTH_SESSION_VARIABLE] = $sessionId;
+    }
+
+    public static function logout(): void
+    {
+        session_destroy();
+    }
+}
