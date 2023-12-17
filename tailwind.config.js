67477e744936cb56b16e735522cee320c2cfa400
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-18 00:58:47 +0100

    TailwindCSS config file creation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/Resources/Css/**/*.css",
+      "./src/Views/**/*.php",
+  ],
+  theme: {
+    extend: {
+      //
+    },
+  },
+  plugins: [],
+}
+
