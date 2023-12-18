107d404569e12242861084ec979872a091297607
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-18 23:02:59 +0100

    Example DB connection file created

---
+++ src/Database/connection_example.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * connection.php
+ * MVCLite by belicfr
+ *
+ * Please edit the connection information
+ * to use your databse properly.
+ */
+
+use MvcLite\Database\Engine\Database;
+use MvcLite\Database\Engine\Exceptions\FailedConnectionToDatabaseException;
+
+const DATABASE_CREDENTIALS = [
+
+    "dbms"          =>  "mysql",
+
+    "host"          =>  "localhost",
+    "port"          =>  "3306",
+    "name"          =>  "",
+    "user"          =>  "",
+    "password"      =>  ""
+
+];
+
+
+$db = (new Database(DATABASE_CREDENTIALS))->attemptConnection();
+
+if (!$db)
+{
+    $error = new FailedConnectionToDatabaseException();
+    $error->render();
+}
