306f668a8b1d529696f00852cc4fccf20f8e0acf
Jonathan <96206878+belicfr@users.noreply.github.com>
2023-12-21 12:36:14 +0100

    PhpDoc + component method

--- src/Engine/InternalResources/Storage.php
+++ src/Engine/InternalResources/Storage.php
@@ -8,22 +8,50 @@ use MvcLite\Engine\InternalResources\Exceptions\NotFoundResourceException;
 
 class Storage
 {
+    /** RegEx for .css files. */
     private const REGEX_CSS_FILE = "/(.*).css$/";
 
+    /** RegEx for .js files. */
     private const REGEX_JS_FILE = "/(.*).js$/";
 
+    /**
+     * @return string /src/Engine/ folder path
+     */
     public static function getEnginePath(): string
     {
-        return $_SERVER['DOCUMENT_ROOT'] . "/src/Engine";
+        return $_SERVER['DOCUMENT_ROOT'] . ROUTE_PATH_PREFIX . "/src/Engine";
     }
 
+    /**
+     * @return string /src/Resources/ folder path
+     */
     public static function getResourcesPath(): string
     {
-        return $_SERVER['DOCUMENT_ROOT'] . "/src/Resources";
+        return $_SERVER['DOCUMENT_ROOT'] . ROUTE_PATH_PREFIX . "/src/Resources";
     }
 
+    /**
+     * @return string /src/Views/Components/ folder path
+     */
+    public static function getComponentsPath(): string
+    {
+        return $_SERVER["DOCUMENT_ROOT"] . ROUTE_PATH_PREFIX . "/src/Views/Components";
+    }
+
+    /**
+     * Attempt to include a supported file in a view.
+     *
+     * @param string $relativePath Relative path to file
+     * @param string $type Optional file type (by default
+     *                     automatically defined)
+     * @return string Generated HTML content
+     */
     public static function include(string $relativePath, string $type = ""): string
     {
+        $pathPrefix = ROUTE_PATH_PREFIX[strlen(ROUTE_PATH_PREFIX) - 1] == '/'
+            ? substr(ROUTE_PATH_PREFIX, 0, strlen(ROUTE_PATH_PREFIX) - 1)
+            : ROUTE_PATH_PREFIX;
+
         $relativePath = $relativePath[0] == '/'
             ? $relativePath
             : '/' . $relativePath;
@@ -31,6 +59,7 @@ class Storage
         $absolutePath = self::getResourcesPath() . $relativePath;
 
         $type = strtolower($type);
+        $html = "";
 
         if (!file_exists($absolutePath))
         {
@@ -40,11 +69,11 @@ class Storage
 
         if ($type == "css" || preg_match(self::REGEX_CSS_FILE, $relativePath))
         {
-            $html = "<link rel='stylesheet' href='/src/Resources$relativePath' />";
+            $html = "<link rel='stylesheet' href='$pathPrefix/src/Resources$relativePath' />";
         }
         else if ($type == "js" || preg_match(self::REGEX_JS_FILE, $relativePath))
         {
-            $html = "<script src='/src/Resources/$relativePath'></script>";
+            $html = "<script src='$pathPrefix/src/Resources/$relativePath'></script>";
         }
         else
         {
@@ -55,5 +84,13 @@ class Storage
         echo $html;
 
         return $html;
+    }
+
+    public static function component(string $name, array $props = []): void
+    {
+        $props["props"] = Delivery::get();
+        extract($props);
+
+        include "src/Views/Components/$name.php";
     }
 }
