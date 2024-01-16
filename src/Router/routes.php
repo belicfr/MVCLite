d150e29e5e3fdac34b819da05abd86882f08638c
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-16 02:32:23 +0100

    Replaced Age example routes by Index ones

--- src/Router/routes.php
+++ src/Router/routes.php
@@ -6,14 +6,13 @@
  */
 
 
+use MvcLite\Controllers\IndexController;
 use MvcLite\Router\Engine\Router;
 
 // Create your own routes!
 
-Router::get("/", \MvcLite\Controllers\AgeController::class, "redirectToIndex");
+Router::get("/", IndexController::class, "redirectToIndex");
 
-Router::get("/age", \MvcLite\Controllers\AgeController::class, "render")
-    ->setName("age");
 
-Router::post("/age", \MvcLite\Controllers\AgeController::class, "ofAge")
-    ->setName("post.age");
+Router::get("/index", IndexController::class, "render")
+    ->setName("index");
