91116954afa9e9fc444b332d49129ec85b107c3e
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-21 22:15:08 +0100

    Created BelongsTo relationship

---
+++ src/Models/Engine/Relationships/BelongsTo.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MvcLite\Models\Engine\Relationships;
+
+use MvcLite\Engine\DevelopmentUtilities\Debug;
+use MvcLite\Engine\Precepts\Naming;
+use MvcLite\Models\Engine\Model;
+use MvcLite\Models\Engine\Relationships\ModelRelationship;
+
+class BelongsTo extends ModelRelationship
+{
+    /** Relationship child. */
+    private ?Model $child;
+
+    /** Foreign key column name. */
+    private string $foreignKeyColumnName;
+
+    public function __construct(Model $leftModel, string $rightModel, ?string $customTableName = null)
+    {
+        parent::__construct($leftModel, $rightModel, $customTableName);
+
+        $this->foreignKeyColumnName
+            = $customColumnName ?? "id_" . Naming::camelToSnake(Naming::getClassName($rightModel));
+    }
+
+    /**
+     * @return Model|null Relationship child
+     */
+    public function getChild(): ?Model
+    {
+        return $this->child;
+    }
+
+    /**
+     * @return string Foreign key column name
+     */
+    public function getForeignKeyColumnName(): string
+    {
+        return $this->foreignKeyColumnName;
+    }
+
+    /**
+     * Returns relationship child.
+     *
+     * @return Model|null
+     */
+    public function run(): ?Model
+    {
+        $related = (new (parent::getRightModel()))
+            ->select()
+            ->where("id", parent::getLeftModel()->{$this->getForeignKeyColumnName()})
+            ->execute();
+
+        return $this->child = $related[0] ?? null;
+    }
+}
