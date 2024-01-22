b5d85587a4dbca85249ae789eae64d683ab4a08d
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-22 10:15:58 +0100

    Clean default User model

--- src/Models/User.php
+++ src/Models/User.php
@@ -6,33 +6,10 @@ use MvcLite\Models\Engine\Model;
 
 class User extends Model
 {
-    /** User id. */
-    private int $id;
-
-    /** Username */
-    private string $name;
-
-    public function __construct(int $id, string $name)
+    public function __construct()
     {
         parent::__construct();
 
-        $this->id = $id;
-        $this->name = $name;
-    }
-
-    /**
-     * @return int User id
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return string Username
-     */
-    public function getName(): string
-    {
-        return $this->name;
+        // Empty constructor.
     }
 }
