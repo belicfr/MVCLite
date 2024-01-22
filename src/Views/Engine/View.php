bde138fff4730a8bcd2d65d73e43c1e651780c75
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-22 13:19:55 +0100

    Added support for Twig for View rendering

--- src/Views/Engine/View.php
+++ src/Views/Engine/View.php
@@ -6,10 +6,38 @@ use MvcLite\Engine\DevelopmentUtilities\Debug;
 use MvcLite\Engine\InternalResources\Delivery;
 use MvcLite\Router\Engine\Request;
 use MvcLite\Views\Engine\Exceptions\NotFoundViewException;
+use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+use Twig\Loader\FilesystemLoader;
 
 class View
 {
     public static function render(string $viewPath, array $props = [])
+    {
+        $absoluteViewPath = "./src/Views/$viewPath.twig";
+
+        if (!file_exists($absoluteViewPath))
+        {
+            $error = new NotFoundViewException();
+            $error->render();
+        }
+
+        $props["delivery"] = Delivery::get();
+
+        echo "<noscript>
+                  Veuillez activer le JavaScript.
+                  <meta http-equiv=\"refresh\" content=\"0; url=" . ROUTE_PATH_PREFIX . "nojs.php\" />
+              </noscript>";
+
+        $html = file_get_contents($absoluteViewPath);
+
+        $twigLoader = new FilesystemLoader("./src/Views");
+        $twigEnvironment = new Environment($twigLoader);
+
+        echo $twigEnvironment->render("$viewPath.twig", $props);
+    }
+
+    public static function nativeRender(string $viewPath, array $props = [])
     {
         $absoluteViewPath = "./src/Views/$viewPath.php";
 
@@ -27,6 +55,6 @@ class View
                   <meta http-equiv=\"refresh\" content=\"0; url=" . ROUTE_PATH_PREFIX . "nojs.php\" />
               </noscript>";
 
-        include_once $absoluteViewPath;
+        include $absoluteViewPath;
     }
 }
