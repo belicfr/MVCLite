29292baea023d37021c5ad7fe2a12cb94925f0af
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-01-22 18:26:51 +0100

    Cleaned

--- src/Router/routes.php
+++ src/Router/routes.php
@@ -10,12 +10,3 @@ use MvcLite\Controllers\IndexController;
 use MvcLite\Router\Engine\Router;
 
 // Create your own routes!
-
-Router::get("/", IndexController::class, "redirectToIndex");
-
-
-Router::get("/index/php", IndexController::class, "renderPhp")
-    ->setName("index.php");
-
-Router::get("/index/twig", IndexController::class, "renderTwig")
-    ->setName("index.twig");
