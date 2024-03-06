e4291d1f12a7194c0f0f46651b4ada584d2d654b
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-03-06 01:16:44 +0100

    Create PluginsManager.php

---
+++ src/Plugins/PluginsManager.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace MvcLite\Plugins;
+
+use MvcliteCore\Engine\DevelopmentUtilities\Debug;
+use MvcliteCore\Plugins\Exceptions\NotFoundPluginException;
+use MvcliteCore\Plugins\Exceptions\PluginAlreadyExistsException;
+
+class PluginsManager
+{
+    private static array $loadedPlugins = [];
+
+    /**
+     * Load all existing enabled plugins.
+     */
+    public static function loadPlugins(): void
+    {
+        foreach (self::getEnabledPlugins() as $pluginName => $plugin)
+        {
+            if (!self::isPluginExists($plugin))
+            {
+                $error = new NotFoundPluginException($pluginName, $plugin);
+                $error->render();
+            }
+
+            if (self::isPluginLoaded($plugin))
+            {
+                $error = new PluginAlreadyExistsException($plugin);
+                $error->render();
+            }
+
+            self::$loadedPlugins[$pluginName] = $plugin;
+        }
+    }
+
+    /**
+     * @return array Enabled plugins array
+     */
+    private static function getEnabledPlugins(): array
+    {
+        return include "plugins.php";
+    }
+
+    /**
+     * @param string $plugin Plugin class name
+     * @return bool If the given plugin class exists
+     */
+    public static function isPluginExists(string $plugin): bool
+    {
+        return class_exists($plugin);
+    }
+
+    /**
+     * @param string $plugin Plugin class name
+     * @return bool If the given plugin class is already
+     *              loaded
+     */
+    public static function isPluginLoaded(string $plugin): bool
+    {
+        return in_array($plugin, array_values(self::getLoadedPlugins()));
+    }
+
+    /**
+     * @return array Loaded plugins array
+     */
+    public static function getLoadedPlugins(): array
+    {
+        return self::$loadedPlugins;
+    }
+
+    /**
+     * @param string $eventMethodName Event plugin method
+     *                                name
+     */
+    public static function loadEvent(string $eventMethodName): void
+    {
+        foreach (self::getLoadedPlugins() as $plugin)
+        {
+            $pluginInstance = new $plugin();
+            $pluginInstance->{$eventMethodName}();
+        }
+    }
+}
